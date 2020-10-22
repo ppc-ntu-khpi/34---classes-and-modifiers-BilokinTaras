02aa6344a3151bb72a4db68963ee7c3695ad1d32
Bilokin Taras <bilokintaras@gmail.com>
2020-10-22 20:34:37 +0300

    NA 5 baliv

---
+++ domain/src/test/CustomerTest.java
@@ -0,0 +1,23 @@
+  
+package test;
+
+import domain.Customer;
+
+public class CustomerTest {
+    
+    public static void main(String args[])
+    {
+        Customer customer = new Customer();
+        
+        customer.displayCustomerInfo();
+        
+        customer.setID(-100);
+        
+        customer.setIsNew(false);
+        
+        customer.setTotal(1000.999F);
+        
+        System.out.println("After change:");
+        customer.displayCustomerInfo();
+    }
+}
